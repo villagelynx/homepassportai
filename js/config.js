92e8c54020fb3345ece559056bdbf4eb3415fbe0
villagelynx <robertexell4@gmail.com>
2026-07-13 21:20:37 -0700

    updates

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /** @readonly */
 import { runtimeConfig } from "./runtime-config.js";
 
-export const APP_VERSION = "0.9.70";
+export const APP_VERSION = "0.9.71";
 
 function resolveAnalyzeApiUrl() {
   if (typeof location === "undefined") return "/api/analyze";
